e22c0c783b457b450960336ddbbc3c0185b41ad6
Luu Tieu Long <luutieulong2203@gmail.com>
2024-01-27 08:43:52 +0700

    User_management BE of administrator

--- src/main/java/dao/DBContext.java
+++ src/main/java/dao/DBContext.java
@@ -27,6 +27,5 @@ public abstract class DBContext<T> {
     public abstract ArrayList<T> list();
     public abstract void delete(T entity);
     public abstract void update(T entity);
-    public abstract void create(T entity);
     public abstract void insert(T entity);
 }
